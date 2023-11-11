153862375236660fbf41c2be413bde92b5cd1bba
codngineer <150457230+codngineer@users.noreply.github.com>
2023-11-11 14:05:36 +0530

    Create LinkList.py

---
+++ LinkList.py
@@ -0,0 +1,15 @@
+#Link List
+
+#Operations
+
+#Insertion
+  #a)- At End of list - O(n)
+  #b)- At Begnining of the List - O(1)
+  #c)- At in between of the list - O(1) - O(n)
+
+#Traversal  O(n)
+
+# Deletion
+   #a)- At End of list - O(n)
+  #b)- At Begnining of the List - O(1)
+  #c)- At in between of the list - O(1) - O(n)
